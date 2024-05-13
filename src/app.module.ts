d69cd5881838fb7602001dce044ef9b00a0ecae0
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2024-05-13 00:17:42 -0300

    Update TypeScript and ESLint settings, and refactor TypeORM configuration
    Made significant updates to TypeScript compiler options for stricter and more advanced checks. Enhanced ESLint configuration with additional plugins and rules to improve code quality and consistency. Also refactored TypeORM configuration to align with new typing rules.
    
    Signed-off-by: Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
 import { getTypeOrmConfigAsync } from './config/typeorm.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.env'] }),
     TypeOrmModule.forRootAsync(getTypeOrmConfigAsync),
   ],
   controllers: [AppController],
